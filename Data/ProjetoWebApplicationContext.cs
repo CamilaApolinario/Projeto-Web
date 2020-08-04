464bce56746cff059d3c0a4c62d68687e25d7f51
Viniciusap <vinicius.ap@live.com>
2020-08-04 18:12:04 -0300

    Other entities and second migration

--- Data/ProjetoWebApplicationContext.cs
+++ Data/ProjetoWebApplicationContext.cs
@@ -14,6 +14,9 @@ namespace ProjetoWebApplication.Data
         {
         }
 
-        public DbSet<ProjetoWebApplication.Models.Department> Department { get; set; }
+        public DbSet<Department> Department { get; set; }
+        public DbSet<Seller> Seller { get; set; }
+        public DbSet<SalesRecord> SalesRecord { get; set; }
+
     }
 }
--- Migrations/20200804203634_OtherEntities.Designer.cs
+++ Migrations/20200804203634_OtherEntities.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ProjetoWebApplication.Data;
 
 namespace ProjetoWebApplication.Migrations
 {
     [DbContext(typeof(ProjetoWebApplicationContext))]
-    partial class ProjetoWebApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20200804203634_OtherEntities")]
+    partial class OtherEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
